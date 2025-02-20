9198ac5fe7a9b67352d848ae52857fa7e754d6dc
Richard Kang <richardkangm@gmail.com>
2025-02-19 22:00:18 -0600

    changed folder names to add part 1.1 and 1.2

--- 1.1/src/Components/Content.jsx
+++ 1.1/src/Components/Content.jsx
@@ -1,12 +1,17 @@
-import Part from './Part'
-
 const Content = (props) => {
     return (
-        <div>
-            <Part name={props.part1} number={props.exercises1}></Part>
-            <Part name={props.part2} number={props.exercises2}></Part>
-            <Part name={props.part3} number={props.exercises3}></Part>
-        </div>
+        <>
+  
+        <p>
+            {props.part1} {props.exercises1}
+        </p>
+        <p>
+            {props.part2} {props.exercises2}
+        </p>
+        <p>
+            {props.part3} {props.exercises3}
+        </p>
+      </>
     )
 }
 
