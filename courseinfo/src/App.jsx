4ee483cf5ed520328493e3d1943208f804908e11
richard.kang <richard.kang@servicenow.com>
2026-02-21 15:47:38 -0800

    completed 1.3

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -1,11 +1,17 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   const Header = (props) => {
     return(
@@ -34,11 +40,12 @@ const App = () => {
       <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
     )
   }
+
   return (
     <div>
       <Header course={course}/>
-      <Content part1={part1} exercises1={exercises1} exercises2={exercises2} part2={part2} part3={part3} exercises3={exercises3} />
-      <Total exercises3={exercises3} exercises1={exercises1} exercises2={exercises2} />
+      <Content part1={part1.name} exercises1={part1.exercises} exercises2={part2.exercises} part2={part2.name} part3={part3.name} exercises3={part3.exercises} />
+      <Total exercises3={part3.exercises} exercises1={part1.exercises} exercises2={part2.exercises} />
     </div>
   )
 }
